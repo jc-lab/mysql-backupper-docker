19d563c2f0702635715bcc83ae074fe5ecc38cdd
jichan <joseph@jc-lab.net>
2020-12-16 08:24:46 +0000

    remove temp file

--- go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -e
-
-sudo docker build --tag temp01 .
-sudo docker run --rm -it -e REMOTE_USER=backupagent-k8s-etcd -e REMOTE_HOST=10.1.4.201 -e REMOTE_PATH=/tmp/backuppedfile.txt temp01 /bin/bash
-
